f8b3d602a30390e6a5b3216d0b03cbcc3e2f2de6
Beachhermit <beachhermit@gmail.com>
2017-08-22 23:33:54 +1000

    initial commit

---
+++ quote_machine/js/qm.js
@@ -0,0 +1,13 @@
+/*$(document).ready(function() {
+    
+        $("#getMessage").on("click", function(){
+          // Only change code below this line.
+          $.getJSON("http://api.forismatic.com/api/1.0/", function(json) {
+            $(".message").html(JSON.stringify(json));
+          });
+          // Only change code above this line.
+        });
+    
+      });
+      */
+//var thisQuote = http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1
