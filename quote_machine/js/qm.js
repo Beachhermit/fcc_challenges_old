f40c71f69dcb1dc1e32d2d8586abc7ff81435416
Beachhermit <beachhermit@gmail.com>
2017-08-31 20:03:09 +1000

    working page

--- quote_machine/js/qm.js
+++ quote_machine/js/qm.js
@@ -1,13 +1,15 @@
-/*$(document).ready(function() {
-    
-        $("#getMessage").on("click", function(){
-          // Only change code below this line.
-          $.getJSON("http://api.forismatic.com/api/1.0/", function(json) {
-            $(".message").html(JSON.stringify(json));
-          });
-          // Only change code above this line.
-        });
-    
+jQuery(document).ready(function() {
+  
+      jQuery("#getQuote").on("click", function(){
+        // Only change code below this line.
+        
+        jQuery.getJSON("https://cors-anywhere.herokuapp.com/" +
+        "https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en", function(json) {
+    jQuery("#quoteText").html(JSON.stringify(json.quoteText));
+    jQuery("#authorText").html(JSON.stringify(json.quoteAuthor));
+  });
+        
+        // Only change code above this line.
       });
-      */
-//var thisQuote = http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1
+  
+    });
