6263e5309ae87b840780761af2c06cb08d911e52
Beachhermit <beachhermit@gmail.com>
2017-09-02 22:15:06 +1000

    working page v1.11

--- quote_machine/js/qm.js
+++ quote_machine/js/qm.js
@@ -5,16 +5,11 @@ $(document).ready(function() {
     $.getJSON("https://cors-anywhere.herokuapp.com/" +
     "https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en", function(json) {
 
-      let quote = JSON.stringify(json.quoteText);
-      let author = JSON.stringify(json.quoteAuthor);
+      let quote = '"' + json.quoteText + '"';
+      let author = json.quoteAuthor || "Anonymous";
       $("#quoteText").html(quote);
-      if (author != "") {
-        $("#authorText").html(author);
-      }
-      else {
-        $("#authorText").html("Anonymous");
-      }
-      
+      $("#authorText").html(author);
+            
     });
 
   });
